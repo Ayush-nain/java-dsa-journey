60cf3502b0ae2b9954976091c8f62d9ccc9eb2b4
Ayush-nain <ayushnainwal135@gmail.com>
2026-07-04 13:26:12 +0530

    Complete Arraylist Declaration , add and get operation

---
+++ Java-Fundamentals/ArrayList/Implementation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Implementation
+{
+    public static void main(String args[])
+    {
+        //Declaration
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        
+        //Insert
+        arr.add(1);
+        arr.add(4);
+        arr.add(2);
+
+        //get
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(2));
+    }
+}
